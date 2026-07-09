3dad856acbb3c1c7fceef86fc1c2c5cf8e6ec6f7
jeffpeng <jeffpeng2012@gmail.com>
2026-07-09 10:20:45 +0800

    cleanup: remove test.c, native-lib.cpp, odas_wrapper from CMakeLists

--- app/src/main/cpp/test.c
+++
@@ -1,9 +0,0 @@
-#include <jni.h>
-
-JNIEXPORT jint JNICALL Java_com_example_myapplication_MainActivity_stringFromCJNI(
-        JNIEnv* env,
-        jobject o /* this */) {
-    char* hello = "Fucking from C";
-//    return (*env)->NewStringUTF(env, hello);
-    return 1234;
-}
